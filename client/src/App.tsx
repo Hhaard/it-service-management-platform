cfe558c53413ae4493557741db38d783d8affc83
Haard Patel <haardp9@gmail.com>
2026-08-26 13:41:07 -0600

    built initial files

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>IT Service Management Platform</h1>
+
+      <p>
+        Manage incidents, service requests, support tickets,
+        and IT operations.
+      </p>
+    </div>
+  );
+}
+
+export default App;
