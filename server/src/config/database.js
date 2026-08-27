36e9b986bcee574a882f7eba8d636e5ee52eabfd
Haard Patel <haardp9@gmail.com>
2026-08-26 18:24:04 -0600

    setting up the database

---
+++ server/src/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/it_service_management"
+    );
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
